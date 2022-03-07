46887b1b6bdc1848c37abc62accbcaaf0cfd2d36
MarineSublet <sublet.marine@gmail.com>
2022-03-07 15:14:08 +0100

    api 2

--- server/api.js
+++ server/api.js
@@ -24,10 +24,27 @@ app.get('/products', async (request, response) => {
   response.send(await  db.find());
 });
 
+
+
+
+app.get('/products/:search', async (request, response) => {
+  await db.connect();
+  var dict={}
+  if(request.query.brand!=null) {dict["brand"]=request.query.brand}
+  if(request.query.price!=null) {dict["price"]=parseInt(request.query.price)}
+  if(request.query.limit!=null) {dict["limit"]=parseInt(request.query.price)}
+  else {dict["limit"]=12}
+  response.send(await  db.find(dict));
+  //{"brand":request.query.brand,"price":parseInt(request.query.price)}
+});
+
 app.get('/products/:id', async (request, response) => {
   await db.connect();
+  console.log(request.params.id)
   response.send(await  db.find({"_id":request.params.id}));
 });
+
+//search?limit=5&brand=montlimart&price=50
 
 app.listen(PORT);
 
